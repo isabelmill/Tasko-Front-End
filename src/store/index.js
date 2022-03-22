7d8d99713d68b9e743877b9f86a631d01c8a4564
Isabel Mill <isabelmavd2@gmail.com>
2022-03-22 18:22:08 +0200

    styles

---
+++ src/store/index.js
@@ -0,0 +1,20 @@
+import {
+    createStore
+  } from 'vuex'
+  import boardStore from './modules/board-module.js'
+  import userStore from './modules/user-module.js'
+  
+  // create a store instance
+  const store = createStore({
+    strict: true,
+    state: {},
+    getters: {},
+    mutations: {},
+    actions: {},
+    modules: {
+        boardStore,
+      userStore,
+    },
+  })
+  
+  export default store
