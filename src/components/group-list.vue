ea6dfb7b245487e13f7190c7ba3bb03710a6c940
EL00SE <elooseghantous@gmail.com>
2022-03-23 23:01:50 +0200

    updating CRUD

--- src/components/group-list.vue
+++ src/components/group-list.vue
@@ -2,7 +2,12 @@
 <template >
     <section class="group-list-main">
         <section v-for="group in groups" :key="group.id" class="group-preview-main">
-            <toggle-input-cmp class="title" @titleChange="changeTitle" :title="group.title" :id="group.id"></toggle-input-cmp>
+            <toggle-input-cmp
+                class="title"
+                @titleChange="changeTitle"
+                :title="group.title"
+                :id="group.id"
+            ></toggle-input-cmp>
 
             <card-preview
                 v-for="card in group.cards"
@@ -19,7 +24,6 @@
 import cardPreview from "./card-preview.vue";
 import toggleInputCmp from "./toggle-input-cmp.vue";
 import addCardCmp from "./add-card-cmp.vue";
-import { boardService } from "../services/board-service.js";
 
 export default {
     name: "group-list",
@@ -41,16 +45,12 @@ export default {
             groupToDisplay: {
                 title: ""
             },
-            newCard: boardService.getEmptyCard(),
         };
     },
-    async created() {
-        const { _id } = this.$route.params
-        this.board = await boardService.getById(_id)
-        this.groupsToShow = this.groups
+    created() {
     },
     methods: {
-        changeTitle(answer) {
+        changeTitle(editedCard) {
             const group = JSON.parse(JSON.stringify(this.board.groups.find(group => group.id === answer.id)))
             group.title = answer.txt
             this.$store.dispatch({ type: 'saveBoard', board: this.board })
@@ -59,14 +59,10 @@ export default {
         close() {
             this.show = false;
         },
-        addNewCard({txt, id}) {
-            this.groupToEdit = JSON.parse(JSON.stringify(this.board.groups.find(group => group.id === id)))
-            this.newCard.title = txt
-            this.groupToEdit.cards.push(this.newCard)
-            // this.groupsToShow = this.board.groups
+        addNewCard(newCard) {
+            this.groupToEdit = JSON.parse(JSON.stringify(this.groups.find(group => group.id === id)))
+            this.groupToEdit.cards.push(newCard)
             this.$emit('groupUpdated', this.groupToEdit)
-            // this.$store.dispatch({ type: 'saveBoard', board: this.board })
-            this.newCard = boardService.getEmptyCard()
         }
     },
     mounted() {
--- src/components/toggle-input-cmp.vue
+++ src/components/toggle-input-cmp.vue
@@ -47,7 +47,6 @@ export default {
     methods: {
         openTitleEdit() {
             this.titleIsOpen = true
-
         },
         change() {
             this.titleIsOpen = false;
