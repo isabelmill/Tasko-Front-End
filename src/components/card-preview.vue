e6e211cad9179d0e24f6fb2d5cd8ea0e45f8c515
EL00SE <elooseghantous@gmail.com>
2022-03-23 11:41:17 +0200

    cardPrev

--- src/components/card-preview.vue
+++ src/components/card-preview.vue
@@ -1,24 +1,39 @@
 <template>
-  <section class="card-preview">
-
-  </section>
+    <section class="card-preview">
+        <p v-if="!titleIsOpen" class="card-title" @click="openTitleEdit">{{ card.title }}</p>
+        <input v-model="cardToDisplay.title" type="text" placeholder="card.title" @submit.prevent="changeTitle"> 
+        </section>
 </template>
 
 <script>
 export default {
-name: 'card-preview',
-props: {
-    card:{
-        type: Object,
-        required:true,
+    name: 'card-preview',
+    props: {
+        card: {
+            type: Object,
+            required: true,
+        },
     },
-},
-methods:{
-
-},
+    data() {
+        return {
+            titleIsOpen: false,
+            cardToDisplay: null,
+        }
+    },
+    created(){
+        this.cardToDisplay=this.card;
+    },
+    methods: {
+        openTitleEdit() {
+            this.titleIsOpen=true;
+        },
+        changeTitle(){
+            
+            this.titleIsOpen=false;
+        }
+    },
 }
 </script>
 
 <style>
-
 </style>
