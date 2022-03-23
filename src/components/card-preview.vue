d347803d282a99e9094cea1b3daec87f15def68f
EL00SE <elooseghantous@gmail.com>
2022-03-23 10:24:15 +0200

    card-prev

---
+++ src/components/card-preview.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="card-preview">
+
+  </section>
+</template>
+
+<script>
+export default {
+name: 'card-preview',
+props: {
+    card:{
+        type: Object,
+    },
+},
+methods:{
+    
+},
+}
+</script>
+
+<style>
+
+</style>
