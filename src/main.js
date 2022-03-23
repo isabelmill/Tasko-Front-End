dc70bff73356a4fae3995caf93a78e9777017928
EL00SE <elooseghantous@gmail.com>
2022-03-24 00:32:01 +0200

    LETS GOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store/index.js'
+import store from './store'
 import "./styles/styles.scss";
 import vClickOutside from 'click-outside-vue3'
 const app = createApp(App)
