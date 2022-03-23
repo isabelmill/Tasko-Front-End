fed7ed94ca11c12d943d20df46a220f1586dc1b0
EL00SE <elooseghantous@gmail.com>
2022-03-23 20:29:41 +0200

    mini-edit

--- src/router/index.js
+++ src/router/index.js
@@ -2,30 +2,35 @@ import { createRouter, createWebHistory } from 'vue-router'
 import home from '../views/home.vue'
 import boardApp from '../views/board-app.vue'
 import boardDetails from '../views/board-details.vue'
+import cardDetails from '../views/card-details.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: home
-    },
-    {
-      path: '/board',
-      name: 'board-app',
-      component: boardApp,
-    },
-    {
-      path: '/board/:_id',
-      name: 'board-details',
-      component: boardDetails,
-    },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    // }
-  ]
+    history: createWebHistory(
+        import.meta.env.BASE_URL),
+    routes: [{
+            path: '/',
+            name: 'home',
+            component: home
+        },
+        {
+            path: '/board',
+            name: 'board-app',
+            component: boardApp,
+        },
+        {
+            path: '/board/:_id',
+            name: 'board-details',
+            component: boardDetails,
+            children: [{
+                path: '/card/',
+                component: cardDetails
+            }]
+        },
+        // {
+        //   path: '/about',
+        //   name: 'about',
+        // }
+    ]
 })
 
 export default router
---
+++ src/views/card-details.vue
@@ -0,0 +1,17 @@
+<template></template>
+
+<script>
+export default {
+    name: 'card-details',
+    created() {
+        // const { cardId } = this.$route.params
+        console.log(this.$route)
+        if (cardId){
+
+        }
+    },
+}
+</script>
+
+<style>
+</style>
