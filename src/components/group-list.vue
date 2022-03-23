ede801713cee28ab370fde58259d22fc6e2bc7ad
EL00SE <elooseghantous@gmail.com>
2022-03-23 14:44:00 +0200

    asdsad

--- src/components/group-list.vue
+++ src/components/group-list.vue
@@ -1,17 +1,35 @@
 <!-- card preview inside group list -->
 <template>
     <section class="group-list">
-        <section v-for="group in groups" :key="group._id" class="group-preview">
-            {{group.title}}
-            <card-preview v-for="card in group.cards" :key="card._id" :groupId="group._id" :card="card"></card-preview>
+        <section v-for="group in groups" :key="group.id" class="group-preview">
+            <p
+                v-if="!titleIsOpen"
+                class="group-title cursor-pointer"
+                @click="openTitleEdit(group.id)"
+            >{{ group.title }}</p>
+            <input
+                v-clickOutside="changeTitle"
+                v-if="titleIsOpen"
+                v-model="groupToDisplay.title"
+                type="text"
+                placeholder="group.title"
+                @submit.prevent="changeTitle"
+            />
+            <card-preview
+                v-for="card in group.cards"
+                :key="card.id"
+                :groupId="group.id"
+                :card="card"
+            ></card-preview>
         </section>
     </section>
 </template>
 
 <script>
-import cardPreview from "./card-preview.vue"
+import cardPreview from "./card-preview.vue";
+import { boardService } from "../services/board-service.js";
 export default {
-    name: 'group-list',
+    name: "group-list",
     components: {
         cardPreview,
     },
@@ -19,17 +37,30 @@ export default {
         groups: {
             type: Array,
             required: true,
-        }
+        },
     },
     data() {
         return {
-            // cards: null,
-            // groups: null,
-        }
+            titleIsOpen: false,
+            groupToDisplay: null,
+            board: null,
+            groupId: null,
+        };
     },
     created() {
-        // this.cards = this.group.cards
-        // console.log(this.group.cards)
+        const { _id } = this.$route.params
+        this.board = boardService.getById(_id)
+    },
+    methods: {
+        openTitleEdit(id) {
+            this.groupId = id
+            this.titleIsOpen = true;
+            this.groupToDisplay = this.board.groups.find(group => group.id === this.groupId)
+        },
+         changeTitle() {
+            this.$store.dispatch({ type: 'saveBoard', board: this.board })
+            this.titleIsOpen = false;
+        }
     },
 }
 </script>
