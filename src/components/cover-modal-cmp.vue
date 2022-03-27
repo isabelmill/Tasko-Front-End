481d25c3bc2e749fed01884265ea5fb44c0abd6d
EL00SE <elooseghantous@gmail.com>
2022-03-27 19:15:19 +0300

    colors

--- src/components/cover-modal-cmp.vue
+++ src/components/cover-modal-cmp.vue
@@ -10,7 +10,6 @@
             <span class="main-title">Cover</span>
         </div>
         <section class="actions-modal-main">
-            <input class="main-input" type="text" />
             <span class="mini-title">Size</span>
             <section class="cover-size-select">
                 <section class="cover-size-small">
@@ -33,17 +32,17 @@
                 </section>
             </section>
             <span class="mini-edit-title">Colors</span>
-            <section class="create-board-background-color">
-                <div @click="setCoverColor('#61BD4F')" class="green color-pref"></div>
-                <div @click="setCoverColor('#F2D600')" class="yellow color-pref"></div>
-                <div @click="setCoverColor('#FF9F1A')" class="orange color-pref"></div>
-                <div @click="setCoverColor('#EB5A46')" class="red color-pref"></div>
-                <div @click="setCoverColor('#C377E0')" class="purple color-pref"></div>
-                <div @click="setCoverColor('#0079BF')" class="blue color-pref"></div>
-                <div @click="setCoverColor('#00C2E0')" class="light-blue color-pref"></div>
-                <div @click="setCoverColor('#51E898')" class="light-green color-pref"></div>
-                <div @click="setCoverColor('#FF78CB')" class="pink color-pref"></div>
-                <div @click="setCoverColor('#344563')" class="dark-blue color-pref"></div>
+            <section class="cover-colors">
+                <div @click="setCoverColor('#61BD4F')" :class="{ selected: color === '#61BD4F' }" class="green color-pref-cover"></div>
+                <div @click="setCoverColor('#F2D600')" :class="{ selected: color === '#F2D600' }" class="yellow color-pref-cover"></div>
+                <div @click="setCoverColor('#FF9F1A')" :class="{ selected: color === '#FF9F1A' }" class="orange color-pref-cover"></div>
+                <div @click="setCoverColor('#EB5A46')" :class="{ selected: color === '#EB5A46' }" class="red color-pref-cover"></div>
+                <div @click="setCoverColor('#C377E0')" :class="{ selected: color === '#C377E0' }" class="purple color-pref-cover"></div>
+                <div @click="setCoverColor('#0079BF')" :class="{ selected: color === '#0079BF' }" class="blue color-pref-cover"></div>
+                <div @click="setCoverColor('#00C2E0')" :class="{ selected: color === '#00C2E0' }" class="light-blue color-pref-cover"></div>
+                <div @click="setCoverColor('#51E898')" :class="{ selected: color === '#51E898' }" class="light-green color-pref-cover"></div>
+                <div @click="setCoverColor('#FF78CB')" :class="{ selected: color === '#FF78CB' }" class="pink color-pref-cover"></div>
+                <div @click="setCoverColor('#344563')" :class="{ selected: color === '#344563' }" class="dark-blue color-pref-cover"></div>
             </section>
             <span class="mini-edit-title">Attachments</span>
             <button type="button" @click="createNewLabel" class="create-btn">Upload a cover image</button>
@@ -74,6 +73,7 @@ export default {
     data() {
         return {
             cardToEdit: null,
+            color:'',
         }
     },
     computed: {
@@ -84,12 +84,12 @@ export default {
         close() {
             this.$emit('actionsClose')
         },
-        setCoverColor(cover) {
+        setCoverColor(color) {
+            this.color = color
 
 
 
-
-            this.$emit('cardEdit', this.cardToEdit)
+            // this.$emit('cardEdit', this.cardToEdit)
 
         },
     },
