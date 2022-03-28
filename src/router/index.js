3a0b147d65345fdb55b915613bf774cd690fdf4e
Anna Balinov <annabalinov@gmail.com>
2022-03-29 00:27:32 +0300

    00:27

--- src/router/index.js
+++ src/router/index.js
@@ -3,26 +3,25 @@ import home from '../views/home.vue'
 import boardApp from '../views/board-app.vue'
 import boardDetails from '../views/board-details.vue'
 
-
 const router = createRouter({
     history: createWebHistory(
         import.meta.env.BASE_URL),
     routes: [{
-            path: '/',
-            name: 'home',
-            component: home
-        },
-        {
-            path: '/board',
-            name: 'board-app',
-            component: boardApp,
-        },
-        {
-            path: '/board/:boardId',
-            name: 'board-details',
-            component: boardDetails,
-        },
-    ]
+        path: '/',
+        name: 'home',
+        component: home
+    },
+    {
+        path: '/board',
+        name: 'board-app',
+        component: boardApp,
+    },
+    {
+        path: '/board/:boardId',
+        name: 'board-details',
+        component: boardDetails,
+    },
+]
 })
 
 export default router
